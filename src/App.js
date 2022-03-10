ece47ff301bf75c49889d086aed305d67425ed3e
Halil Demirel <halil.demirel@turkcell.com.tr>
2022-03-10 18:28:01 +0300

    Initial commit to Repo

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Halil You are Great Man. Your Eager to learn life is the best.
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
